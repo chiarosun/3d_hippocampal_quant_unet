6acf85e5b60722b33335977e22d69ce021d9cd80
magellan <chiarosun@gmail.com>
2020-10-04 17:48:27 -0700

    updating dirs

---
+++ deployment/deploy_scripts/send_volume.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# This script sends a study to the Orthanc server
+storescu 127.0.0.1 4242 -v -aec HIPPOAI +r +sd /data/TestVolumes/Study1
--- deployment/src/deploy_scripts/send_volume.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This script sends a study to the Orthanc server
-storescu 127.0.0.1 4242 -v -aec HIPPOAI +r +sd /data/TestVolumes/Study1
